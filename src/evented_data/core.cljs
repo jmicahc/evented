c5d3403a473ddef6a74044cba332e32591a2a83d
John Collins <jmicahc@gmail.com>
2017-01-26 17:22:15 -0800

    rename files

--- src/evented_data/core.cljs
+++ src/evented_data/core.cljs
@@ -1,13 +1,30 @@
 (ns evented_data.core
-  (:require [evented_data.protocols :as protocols
+  (:require [evented_data.collections :as collections
              :refer [diff noun events]]))
 
 
-(defn emap [k v & kvs]
-  (apply assoc (protocols/EventMap. {} [] []) k v kvs))
+(defn emap 
+  ([]
+   (protocols/EventMap. {} [] []))
+  ([k v]
+   (assoc (protocols/EventMap. {} [] []) k v))
+  ([k1 v1 k2 v2]
+   (assoc (protocols/EventMap. {} [] []) k1 v1 k2 v2))
+  ([k1 v1 k2 v2 k3 v3]
+   (assoc (protocols/EventMap. {} [] []) k1 v1 k2 v2 k3 v3))
+  ([k1 v1 k2 v2 k3 v3 & kvs])
+   (apply assoc (emap k1 v1 k2 v2 k3 v3) kvs))
 
 
-(defn evec [x & xs]
-  (apply conj (protocols/EventVector. [] [] []) x xs))
+
+(defn evec
+  ([]
+   (protocols/EventVector. [] [] []))
+  ([x]
+   (conj (protocols/EventVector. [] [] []) x))
+  ([x y]
+   (conj (protocols/EventVector. [] [] []) x y))
+  ([x y & xs]
+   (apply conj (protocols/EventVector. [] [] []) x y xs)))
 
 
